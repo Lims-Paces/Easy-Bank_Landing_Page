e2d9d1d623d782cfd85d068c8d897e1aef3dcb0f
Lims-Paces <95597959+Lims-Paces@users.noreply.github.com>
2022-07-19 11:09:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const body = document.querySelector('body')
 const header = document.querySelector('.header')
 const overlay = document.querySelector('.overlay')
 const fadeElems = document.querySelectorAll('.has-fade')
+const phoneMenu = document.querySelectorAll('.phoneMenu')
 
 btnHamburger.addEventListener('click', function() {
     if (header.classList.contains('open')) {
@@ -22,4 +23,11 @@ btnHamburger.addEventListener('click', function() {
             element.classList.add('fade-in')
         })
     }
+})
+
+// remove overlay when the menu links are cliked
+phoneMenu.addEventListener('onclick', function() {
+    overlay.forEach(function(over) {
+        over.classList.remove('overlay')
+    })
 })
